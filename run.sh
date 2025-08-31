75ef433ebeb189fbe2fb2872bcbefd5fe8c9db0d
Raoul Zwart <r.zwart@rdc.nl>
2025-08-31 23:47:31 +0200

    azurite connection string

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 id=1234 ./build.sh -b
-docker run --rm -p 8082:80 -it openfunc:1234
+docker run --rm -p 8082:80 -e AzureWebJobsStorage='DefaultEndpointsProtocol=http;AccountName=account1;AccountKey=key1;BlobEndpoint=http://account1.blob.localhost:10000;QueueEndpoint=http://account1.queue.localhost:10001;TableEndpoint=http://account1.table.localhost:10002;' -it openfunc:1234
 # curl -v http://127.0.01:8082/api/HttpExample
 
